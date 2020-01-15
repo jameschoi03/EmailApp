e2358d3b4a96540ce323724ff6a45d6633645c85
James Choi <jameschoi03@gmail.com>
2020-01-15 01:55:58 -0500

    first commit

---
+++ src/main/java/EmailApp/EmailApp.java
@@ -0,0 +1,11 @@
+package EmailApp;
+
+public class EmailApp {
+
+    public static void main (String [] args){
+        Email email = new Email("James", "Choi");
+
+        System.out.println(email.showInfo());
+
+    }
+}
